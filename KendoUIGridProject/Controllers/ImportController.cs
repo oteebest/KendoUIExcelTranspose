70aaa0ee7326c6081f733f8e41abfb894070a6be
Otee Ziregbe <otee.ziregbe@cyberspace.net.ng>
2021-05-10 19:03:40 +0100

    initialCommit

---
+++ KendoUIGridProject/Controllers/ImportController.cs
@@ -0,0 +1,27 @@
+﻿using KendoUIGridProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoUIGridProject.Controllers
+{
+    public class ImportController : Controller
+    {
+        // GET: Import
+        public ActionResult Index()
+        {
+            return View("ImportFinancials");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Financials(List<Financials> model)
+        {
+
+            return Json(new { success = true, message = "Saved successfully" });
+
+        }
+    }
+}
---
+++ KendoUIGridProject/Models/Financials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KendoUIGridProject.Models
+{
+    public class Financials
+    {
+        public Guid Id { get; set; }
+        public string Year { get; set; }
+        public decimal ? Sales { get; set; }
+        public decimal ? Costs { get; set; }
+        public decimal ? Profit { get; set; }
+    }
+}
